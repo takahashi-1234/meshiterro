83f43b1fbbc117e650660e055bf77563fb2c0935
takahashi-1234 <riki.1130@icloud.com>
2021-10-04 08:31:07 +0000

    [update]コメント機能追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,11 @@
 class PostImage < ApplicationRecord
 
   belongs_to :user
-  has_many:post_comments,dependent: :delete
+  has_many:post_comments,dependent: :destroy
   attachment :image
+  has_many:favorites,dependent: :destroy
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
 end
